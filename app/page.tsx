86c68055044881fa1d3d8d86d8b97273e7036783
Amanda Victoria <amanda.melquiades@netra.global>
2025-10-16 13:42:43 -0300

    feat: incluindo patrocinadores

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default function HomePage() {
         <AboutSection />
         <SpeakersSection />
         {/* <AgendaSection /> */}
-        {/* <SponsorsSection /> */}
+        <SponsorsSection />
         <LocationSection />
         <OrganizersSection />
       </main>
