01b85ac2f2182578d8c96166f5c5d9c662bc2b4b
Amanda Victoria <amandamelquiades01@gmail.com>
2025-10-24 09:24:44 -0300

    feat: incluindo video no hero section

--- components/video-player.tsx
+++ components/video-player.tsx
@@ -1,10 +1,16 @@
 export default function VideoPlayer() {
   return (
-    <div className="w-full flex justify-center items-center p-4 ">
-      <video controls loop className="rounded-lg shadow-md max-w-full" style={{ maxWidth: '1000px', width: '100%', paddingTop: '20px' }}>
-        <source src="/video/DevFest2025Opener.mp4" type="video/mp4" />
-        Seu navegador não suporta a tag de vídeo.
-      </video>
+    <div className="w-full flex justify-center items-center p-4">
+      <iframe
+        width="1000"
+        height="562"
+        src="https://www.youtube.com/embed/Wr0RiuX9dLY"
+        title="DevFest2025 Opener captions Brazilian Portuguese"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+        className="rounded-lg shadow-md max-w-full"
+        style={{ paddingTop: '20px' }}
+      ></iframe>
     </div>
   );
 }
